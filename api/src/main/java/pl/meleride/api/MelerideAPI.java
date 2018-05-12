e68beee8aac8420b0a6fe84027454404a5e9ef6e
Kacper Duras <git@kacperduras.pl>
2018-05-12 21:39:17 +0200

    Usunięcie silnika Paper jako moduł, reorganizacja (#15)
    * Drop Paper
    
    * Remove Paper build script from Travis settings

--- api/src/main/java/pl/meleride/api/MelerideAPI.java
+++ api/src/main/java/pl/meleride/api/MelerideAPI.java
@@ -1,6 +1,20 @@
 package pl.meleride.api;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.meleride.commands.Commands;
+import pl.meleride.commands.bukkit.BukkitCommands;
 
-public class MelerideAPI extends JavaPlugin {
+public final class MelerideAPI extends JavaPlugin {
+
+  private Commands commands;
+
+  @Override
+  public void onEnable() {
+    this.commands = new BukkitCommands(this);
+  }
+
+  public Commands getCommands() {
+    return commands;
+  }
+
 }
