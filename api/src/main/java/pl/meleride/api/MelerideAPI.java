7c09d7b41fdf9d0ba4df195d053eb0ee69c94ea2
bmstefanski <bstefanskicom@gmail.com>
2018-05-15 19:21:58 +0200

    Wersja produkcyjnya

--- api/src/main/java/pl/meleride/api/MelerideAPI.java
+++ api/src/main/java/pl/meleride/api/MelerideAPI.java
@@ -1,22 +1,16 @@
 package pl.meleride.api;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import pl.meleride.api.impl.i18n.MessageBundle;
 import pl.meleride.api.impl.listener.PlayerLoginListener;
 import pl.meleride.api.impl.listener.PlayerPreLoginListener;
 import pl.meleride.api.impl.listener.PlayerQuitListener;
 import pl.meleride.api.impl.manager.UserManagerImpl;
-import pl.meleride.api.impl.type.MessageType;
 import pl.meleride.api.manager.UserManager;
 import pl.meleride.commands.Commands;
 import pl.meleride.commands.bukkit.BukkitCommands;
 
-public final class MelerideAPI extends JavaPlugin implements CommandExecutor {
+public final class MelerideAPI extends JavaPlugin {
 
   private UserManager userManager;
   private Commands commands;
@@ -32,36 +26,13 @@ public final class MelerideAPI extends JavaPlugin implements CommandExecutor {
         new PlayerQuitListener(this)
     );
 
-    this.getCommand("test").setExecutor(this);
-
+    this.saveDefaultConfig();
   }
 
   private void registerListeners(Listener... listeners) {
     for (Listener listener : listeners) {
       this.getServer().getPluginManager().registerEvents(listener, this);
     }
-  }
-
-  @Override
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    MessageBundle.create("duzy.chuj")
-        .withField("CHUJ", "DUZY CHUJ")
-        .withField("TEST", "DUZY TEST")
-        .target(MessageType.CHAT)
-        .sendTo((Player) sender);
-
-    MessageBundle.create("duzy.chuj")
-        .withField("TEST", "DUZY TEST")
-        .target(MessageType.ACTION_BAR)
-        .sendTo((Player) sender);
-
-    MessageBundle.create("duzy.chuj")
-        .withField("TEST", "DUZY TEST")
-        .target(MessageType.SUB_TITLE)
-        .sendTo((Player) sender);
-
-    System.out.println(MessageBundle.create("duzy.chuj").withField("TEST", "DUZY TEST").toString());
-    return false;
   }
 
   public Commands getCommands() {
--- api/src/main/java/pl/meleride/api/impl/i18n/MessageBuilder.java
+++ api/src/main/java/pl/meleride/api/impl/i18n/MessageBuilder.java
@@ -2,25 +2,15 @@ package pl.meleride.api.impl.i18n;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
-import pl.meleride.api.impl.type.LocaleType;
+import org.bukkit.plugin.java.JavaPlugin;
 import pl.meleride.api.impl.type.MessageType;
-
-import java.util.ResourceBundle;
 
 public final class MessageBuilder {
 
-  private final static ResourceBundle RESOURCE_BUNDLE;
-
-  private final String key;
   private String messageContent;
 
-  static {
-    RESOURCE_BUNDLE = ResourceBundle.getBundle("messages", LocaleType.DEFAULT.getLocale());
-  }
-
-  MessageBuilder(String key) {
-    this.key = key;
-    this.messageContent = RESOURCE_BUNDLE.getString(key);
+  MessageBuilder(JavaPlugin plugin, String key) {
+    this.messageContent = plugin.getConfig().getString(key);
   }
 
   public MessageBuilder withField(String field, String value) {
