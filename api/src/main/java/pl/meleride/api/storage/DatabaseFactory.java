c6fbb89940cfff0d02540627b54e34f075a1541b
bmstefanski <bstefanskicom@gmail.com>
2018-05-30 22:35:22 +0200

    Przeniesiono dane do autentykacji do pliku konfiguracyjnego

--- api/src/main/java/pl/meleride/api/storage/DatabaseFactory.java
+++ api/src/main/java/pl/meleride/api/storage/DatabaseFactory.java
@@ -4,12 +4,27 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.meleride.api.MelerideAPI;
 import pl.meleride.api.exception.UnknownDatabaseTypeException;
+import pl.meleride.api.i18n.MessageBundler;
 
 import java.util.Optional;
 
 public final class DatabaseFactory {
 
   private static final MelerideAPI PLUGIN = JavaPlugin.getPlugin(MelerideAPI.class);
+
+  private static final String DATABASE_NAME;
+  private static final String SERVER_NAME;
+  private static final String USERNAME;
+  private static final String PASSWORD;
+  private static final int PORT;
+
+  static {
+    DATABASE_NAME = MessageBundler.create("database.mysql.databaseName").toString();
+    SERVER_NAME = MessageBundler.create("database.mysql.serverName").toString();
+    USERNAME = MessageBundler.create("database.mysql.username").toString();
+    PASSWORD = MessageBundler.create("database.mysql.password").toString();
+    PORT = Integer.valueOf(MessageBundler.create("database.mysql.port").toString());
+  }
 
   public static Database getDatabase(String databaseName) {
     Validate.notNull(databaseName, "Database type's name cannot be null");
@@ -18,11 +33,11 @@ public final class DatabaseFactory {
     if (databaseName.equalsIgnoreCase("mysql")) {
       optionalDatabase = Optional.of(new MysqlDatabase(
         PLUGIN.getExecutorService(),
-        "meleride",
-        "localhost",
-        "root",
-        "root",
-        3306)); // until config will be implemented
+        DATABASE_NAME,
+        SERVER_NAME,
+        USERNAME,
+        PASSWORD,
+        PORT));
     }
 
     return optionalDatabase.orElseThrow(UnknownDatabaseTypeException::new);
