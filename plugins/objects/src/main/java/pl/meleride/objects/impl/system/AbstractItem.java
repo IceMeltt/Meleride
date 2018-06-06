e7956907519157956364c11f3171770377d707a8
Tymoteusz Boba <IceMeltWasHere@gmail.com>
2018-06-06 16:36:48 +0200

    Wdrożenie systemu interaktywnych obiektów - itemy. (#31)

---
+++ plugins/objects/src/main/java/pl/meleride/objects/impl/system/AbstractItem.java
@@ -0,0 +1,29 @@
+package pl.meleride.objects.impl.system;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class AbstractItem implements Listener {
+
+  private String name;
+  private ItemStack itemStack;
+
+  protected AbstractItem(String name) {
+    this.name = name.toLowerCase();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public ItemStack getItemStack() {
+    return this.itemStack;
+  }
+
+  @EventHandler
+  public void onPlayerInteract(PlayerInteractEvent e) {
+  }
+
+}
