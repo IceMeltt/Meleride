79cedf454bd117dcf25075a353ba794ac55c1656
IceMeltt <IceMeltWasHere@gmail.com>
2018-06-14 22:04:39 +0200

    Poprawienie odczytu ItemStack'a przy otwieraniu inventory ze sklepem. + misc

--- plugins/base/src/main/java/pl/meleride/base/MelerideBase.java
+++ plugins/base/src/main/java/pl/meleride/base/MelerideBase.java
@@ -33,5 +33,5 @@ public class MelerideBase extends JavaPlugin {
   public DrugShop getDrugShop() {
     return drugShop;
   }
-  
+
 }
--- plugins/base/src/main/java/pl/meleride/base/impl/drug/DrugShop.java
+++ plugins/base/src/main/java/pl/meleride/base/impl/drug/DrugShop.java
@@ -3,7 +3,8 @@ package pl.meleride.base.impl.drug;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 
-import pl.meleride.base.drug.DrugFactory;
+import pl.meleride.objects.impl.system.ItemRegistrator;
+
 import static pl.meleride.api.message.MessageUtil.colored;
 
 public class DrugShop {
@@ -12,10 +13,10 @@ public class DrugShop {
 
   public void initialize() {
     inventory = Bukkit.createInventory(null, 6 * 9, colored("&2Dealer narkotykow"));
-    inventory.setItem(37, DrugFactory.getDrugByName("§2Marihuana").getItemStack());
-    inventory.setItem(39, DrugFactory.getDrugByName("§bEcstazy").getItemStack());
-    inventory.setItem(41, DrugFactory.getDrugByName("Kokaina").getItemStack());
-    inventory.setItem(43, DrugFactory.getDrugByName("Heroina").getItemStack());
+    inventory.setItem(37, ItemRegistrator.getObjectMap().get("drug_cannabis").getItemStack());
+    inventory.setItem(39, ItemRegistrator.getObjectMap().get("drug_mdma").getItemStack());
+    inventory.setItem(41, ItemRegistrator.getObjectMap().get("drug_cocaine").getItemStack());
+    inventory.setItem(43, ItemRegistrator.getObjectMap().get("drug_heroine").getItemStack());
   }
 
   public Inventory getInventory() {
