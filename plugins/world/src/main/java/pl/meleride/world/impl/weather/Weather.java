5a5e3a79a4b458805dfef34fc053d9677e7b0186
IceMeltt <IceMeltWasHere@gmail.com>
2018-06-21 14:33:30 +0200

    W sumie, getter nie jest tu potrzebny.

--- plugins/world/src/main/java/pl/meleride/world/impl/weather/Weather.java
+++ plugins/world/src/main/java/pl/meleride/world/impl/weather/Weather.java
@@ -17,14 +17,10 @@ public class Weather {
   private String newForecast = "Brak informacji";
   private String olderForecast = "Brak informacji";
 
-  private static final String UPDATE_URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Warsaw%22)%20and%20u%3D'c'&format=json";
+  private final String UPDATE_URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Warsaw%22)%20and%20u%3D'c'&format=json";
 
   public static String translate(String input) {
     return MessageBundler.create("forecast." + input).toString();
-  }
-  
-  public static String getUpdateUrl() {
-    return UPDATE_URL;
   }
 
   public int getTemperature() {
