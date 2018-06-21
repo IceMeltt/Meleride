cd532f0138a67bada2f9526f1b16e06dda96fcc2
Tymoteusz Boba <IceMeltWasHere@gmail.com>
2018-06-21 22:19:33 +0200

    Wdrożenie systemu pogody na serwer. (#44)

---
+++ plugins/world/src/main/java/pl/meleride/world/MelerideWorld.java
@@ -0,0 +1,32 @@
+package pl.meleride.world;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import pl.meleride.world.impl.runnable.PlayerCheckAboveRunnable;
+import pl.meleride.world.impl.runnable.WeatherUpdaterRunnable;
+import pl.meleride.world.impl.weather.Weather;
+
+public class MelerideWorld extends JavaPlugin {
+
+  private Weather weather;
+
+  @Override
+  public void onEnable() {
+    getLogger().info("Trwa uruchamianie schedulerow...");
+    weather = new Weather();
+
+    new WeatherUpdaterRunnable(this).runTaskTimerAsynchronously(this, 0, 10 * 60 * 20);
+    new PlayerCheckAboveRunnable(this).runTaskTimerAsynchronously(this, 0, 60 * 20);
+  }
+
+  @Override
+  public void onDisable() {
+    getLogger().info("Zamykanie wszelkich schedulerow...");
+    getServer().getScheduler().cancelAllTasks();
+  }
+
+  public Weather getWeatherInstance() {
+    return weather;
+  }
+
+}
---
+++ plugins/world/src/main/java/pl/meleride/world/impl/weather/Weather.java
@@ -0,0 +1,61 @@
+package pl.meleride.world.impl.weather;
+
+import org.bukkit.Bukkit;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import pl.meleride.api.i18n.MessageBundler;
+import pl.meleride.api.message.MessageType;
+
+public class Weather {
+
+  private int actualTemp = -1;
+  private String newForecast = "Brak informacji";
+  private String olderForecast = "Brak informacji";
+
+  public static final String UPDATE_URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Warsaw%22)%20and%20u%3D'c'&format=json";
+
+  public static String translate(String input) {
+    return MessageBundler.create("forecast." + input).toString();
+  }
+
+  public int getTemperature() {
+    return this.actualTemp;
+  }
+
+  public String getNewerForecast() {
+    return this.newForecast;
+  }
+
+  public String getOlderForecast() {
+    return this.olderForecast;
+  }
+
+  public void updateWeather() throws UnirestException {
+    HttpResponse<JsonNode> response = Unirest.get(UPDATE_URL).asJson();
+
+    JSONObject template = response.getBody().getObject()
+        .getJSONObject("query")
+        .getJSONObject("results")
+        .getJSONObject("channel")
+        .getJSONObject("item")
+        .getJSONObject("condition");
+
+    this.actualTemp = Integer.parseInt(template.get("temp").toString());
+
+    this.olderForecast = this.newForecast;
+    this.newForecast = template.get("text").toString();
+
+    if (!this.newForecast.equalsIgnoreCase(this.olderForecast)) {
+      MessageBundler.create("forecast.newforecast")
+          .withField("FORECAST", this.newForecast)
+          .target(MessageType.CHAT)
+          .sendTo(Bukkit.getOnlinePlayers());
+    }
+  }
+
+}
