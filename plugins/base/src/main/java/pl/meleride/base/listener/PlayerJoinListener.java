3026d124e3da8c0f654ba9c96593d3bae19bbec9
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-08-09 21:38:38 +0200

    Dodano animację podczas wejścia gracza (#71)

--- plugins/base/src/main/java/pl/meleride/base/listener/PlayerJoinListener.java
+++ plugins/base/src/main/java/pl/meleride/base/listener/PlayerJoinListener.java
@@ -1,11 +1,15 @@
 package pl.meleride.base.listener;
 
+import java.util.List;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 import pl.meleride.api.helper.Listener;
 import pl.meleride.base.MelerideBase;
 import pl.meleride.base.entity.User;
+import pl.meleride.base.task.TitleAnimationTask;
+
+import static pl.meleride.api.message.MessageUtil.*;
 
 public class PlayerJoinListener implements Listener<PlayerJoinEvent> {
 
@@ -24,11 +28,19 @@ public class PlayerJoinListener implements Listener<PlayerJoinEvent> {
       user.setName(event.getPlayer().getName());
     }
 
-    if(!event.getPlayer().hasPlayedBefore()) {
+    if (!event.getPlayer().hasPlayedBefore()) {
       this.plugin.getUserResource().save(user);
     } else {
       this.plugin.getUserResource().load(user);
     }
+
+    List<String> titleAnimationContent = colored(
+        this.plugin.getConfig().getStringList("animation.title"));
+    List<String> subTitleAnimationContent = colored(
+        this.plugin.getConfig().getStringList("animation.subtitle"));
+
+    new TitleAnimationTask(titleAnimationContent, subTitleAnimationContent, event.getPlayer(), 0)
+        .runTaskTimerAsynchronously(this.plugin, 20, 4);
   }
 
 }
