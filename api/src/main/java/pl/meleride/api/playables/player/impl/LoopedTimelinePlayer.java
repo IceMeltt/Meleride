dee8ebfcbb364562619f1328f4a9dc8267371806
Larry, the Setup Wizzard <szejkdev@gmail.com>
2018-08-14 16:32:55 +0200

    Modyfikacja systemu efektów (#74)

---
+++ api/src/main/java/pl/meleride/api/playables/player/impl/LoopedTimelinePlayer.java
@@ -0,0 +1,64 @@
+package pl.meleride.api.playables.player.impl;
+
+import pl.meleride.api.playables.PlayableStatus;
+import pl.meleride.api.playables.TimeUnit;
+import pl.meleride.api.playables.drivers.TimelineDriver;
+import pl.meleride.api.playables.player.TimelinePlayer;
+
+public class LoopedTimelinePlayer implements TimelinePlayer {
+  
+  private static final TimeUnit incrementValue = TimeUnit.ticks(1);
+  
+  private final TimelineDriver timelineDriver;
+  private TimeUnit currentTime;
+  private PlayableStatus status;
+  
+  public LoopedTimelinePlayer(TimelineDriver timelineDriver) {
+    this.timelineDriver = timelineDriver;
+    this.currentTime = TimeUnit.zero();
+  }
+  
+  public TimelineDriver getTimelineDriver() {
+    return timelineDriver;
+  }
+  
+  public TimeUnit getCurrentTime() {
+    return currentTime;
+  }
+  
+  @Override
+  public void update() {
+    if (this.status != PlayableStatus.PLAYING)
+      return;
+    
+    this.timelineDriver.handleEvent(this.currentTime);
+    if (this.currentTime.equals(this.timelineDriver.getTimelineLength())) {
+      this.currentTime = TimeUnit.zero();
+    } else {
+      this.currentTime.add(incrementValue);
+    }
+  }
+  
+  @Override
+  public void setStatus(PlayableStatus status) {
+    switch (status) {
+      case PLAYING:
+        if (this.status == PlayableStatus.STOPPED)
+          this.currentTime = TimeUnit.zero();
+      case STOPPED:
+      case PAUSED:
+        this.status = status;
+    }
+  }
+  
+  @Override
+  public PlayableStatus getStatus() {
+    return this.status;
+  }
+  
+  @Override
+  public boolean hasFinished() {
+    return false;
+  }
+  
+}
